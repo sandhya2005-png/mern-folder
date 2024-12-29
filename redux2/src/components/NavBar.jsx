de9f3a5156f6aa4e821f9f4104b7e786c476d497
sandhya2005-png <146368121+sandhya2005-png@users.noreply.github.com>
2024-12-29 23:26:24 +0530

    Update NavBar.jsx

--- redux2/src/components/NavBar.jsx
+++ redux2/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../redux/userSlice'
-const Navbar = () => {
+const Navbar= () => {
     const userdata = useSelector(selectUser)
     return (
         <div>
